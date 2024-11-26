342f19843e10e02756673235d8321206886a93ca
vsantos1711 <vsantos067100@gmail.com>
2024-11-26 15:05:51 -0300

    feat: implement Lambda function for URL shortening and S3 storage

--- url-shortener/src/main/java/com/vsantos/urlshortener/Main.java
+++ url-shortener/src/main/java/com/vsantos/urlshortener/Main.java
@@ -1,7 +1,53 @@
 package com.vsantos.urlshortener;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+    private final S3Client s3Client = S3Client.builder().build();
+
+    @Override
+    public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
+
+        String body = input.get("body").toString();
+        UrlData urlData;
+
+        try {
+            urlData = mapper.readValue(body, UrlData.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Error parsing request body: " + e.getMessage(), e);
+        }
+
+        String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
+
+        try {
+            String urlDataJson = mapper.writeValueAsString(urlData);
+
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket("url-shortener-s3bucket")
+                    .key(shortUrlCode + ".json")
+                    .build();
+
+            s3Client.putObject(putObjectRequest, RequestBody.fromString(urlDataJson));
+        } catch (Exception e) {
+            throw new RuntimeException("Error saving data to S3: " + e.getMessage(), e);
+        }
+
+        Map<String, String> response = new HashMap<>();
+
+        response.put("code", shortUrlCode);
+
+        return response;
     }
 }
