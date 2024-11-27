ad40a98113334900ac6c697edb2d8d82c0dcf7a5
vsantos1711 <vsantos067100@gmail.com>
2024-11-26 21:40:58 -0300

    feat: implement AWS Lambda handler for URL redirection with S3 integration

--- redirect-short-url/src/main/java/com/vsantos/redirectshorturl/Main.java
+++ redirect-short-url/src/main/java/com/vsantos/redirectshorturl/Main.java
@@ -1,7 +1,68 @@
 package com.vsantos.redirectshorturl;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.io.InputStream;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+
+public class Main implements RequestHandler<Map<String, Object>, Map<String, Object>> {
+
+    private final S3Client s3Client = S3Client.builder().build();
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
+        String shortCodeUrl = extractShortUrl(input);
+        UrlData urlData = fetchUrlData(shortCodeUrl);
+
+        return buildResponse(urlData);
     }
+
+    private String extractShortUrl(Map<String, Object> input) {
+        String pathParameters = input.get("rawPath").toString();
+        if (pathParameters == null || pathParameters.isEmpty()) {
+            throw new IllegalArgumentException("Invalid input: 'rawPath' is required");
+        }
+        return pathParameters.replace("/", "");
+    }
+
+    private UrlData fetchUrlData(String shortCodeUrl) {
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket("url-shortener-s3bucket")
+                .key(shortCodeUrl + ".json")
+                .build();
+
+        try (InputStream s3InputStream = s3Client.getObject(getObjectRequest)) {
+            return mapper.readValue(s3InputStream, UrlData.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Error fetching or parsing URL data from S3: " + e.getMessage(), e);
+        }
+    }
+
+    private Map<String, Object> buildResponse(UrlData urlData) {
+        Map<String, Object> response = new HashMap<>();
+        Map<String, Object> headers = new HashMap<>();
+
+        if (urlData.getExpirationDate().before(new Date())) {
+            response.put("statusCode", 410);
+            response.put("body", "This URL has expired!");
+
+            return response;
+        }
+
+        headers.put("Location", urlData.getOriginalUrl());
+
+        response.put("statusCode", 302);
+        response.put("headers", headers);
+        return response;
+
+    }
+
 }
